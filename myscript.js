2cc60740bc93456540d8a592ad5182998290aead
my_username <my_email_address>
2022-10-27 13:03:40 +0300

    Wrote m/i/l

--- myscript.js
+++ myscript.js
@@ -1,4 +1,49 @@
 function main()
 {
     console.log("Hello, world");
+    //Getting Into Position//
+    penup()
+    width(5)
+    left(90)
+    forward(200)
+    left(90)
+    forward(200)
+    right(180)
+    pendown()
+
+    //M//
+    forward(150)
+    right(135)
+    forward(75)
+    left(90)
+    forward(75)
+    right(135)
+    forward(150)
+
+    
+    penup()
+    left(90)
+    forward(50)
+    left(90)
+    pendown()
+
+    //i//
+    forward(75)
+
+    penup()
+    forward(40)
+    pendown()
+
+    //dot of i//
+    forward(20)
+
+    penup()
+    right(180)
+    forward(135)
+    left(90)
+    forward(50)
+    pendown()
+
+    left(90)
+    forward(150)
 }
