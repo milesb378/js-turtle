c8f39f88168d4a177a98ecc7f0b20607605d09cf
my_username <my_email_address>
2022-11-02 09:04:27 +0300

    started ex

--- ex.js
+++ ex.js
@@ -1,4 +1,11 @@
 function main()
 {
-    
+    forward(100);
+    //https://www.w3resource.com/javascript-exercises/javascript-math-exercise-35.php//
+    console.log(pythagorean(200, 300));
+}
+
+function pythagorean(sideA, sideB)
+{
+    return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
 }
